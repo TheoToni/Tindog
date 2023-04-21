6a30883bbd2e68222f9a7ad33adc02d38b7f6ba3
TheoToni <32997269+TheoToni@users.noreply.github.com>
2023-04-21 08:36:14 +0200

    Setting up the class

--- data.js
+++ data.js
@@ -25,4 +25,4 @@ const dogs = [
   },
 ];
 
-// Remember to export the data!
+export default dogs;
--- dog.js
+++ dog.js
@@ -1,1 +1,9 @@
 // Create the Dog class here
+
+import dogs from "./data";
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+}
